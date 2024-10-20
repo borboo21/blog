c1ac7318380f6227e3dedd3395c6c687113ad000
borboo21 <nikich621@gmail.com>
2024-10-20 17:20:58 +0300

    Реализовать регистрацию на сервере

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { register } = require('./controllers/user');
+const mapUser = require('./helpers/mapUser');
 
 const port = 3001;
 
@@ -9,9 +11,19 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
+app.post('/register', async (req, res) => {
+	try {
+		const user = await register(req.body.login, req.body.password);
+
+		res.send({ error: null, user: mapUser(user) });
+	} catch (error) {
+		res.send({ error: error.message || 'Unknown error' });
+	}
+});
+
 mongoose
 	.connect(
-		'mongodb+srv://bor_boo:qwerty123@cluster0.trqbh.mongodb.net/notes?retryWrites=true&w=majority&appName=Cluster0',
+		'mongodb+srv://bor_boo:qwerty123@cluster0.trqbh.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0',
 	)
 	.then(() => {
 		app.listen(port, () => {
--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,5 +1,18 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
 // register
 
+async function register(login, password) {
+	if (!password) {
+		throw new Error('Password is empty');
+	}
+	const passwordHash = await bcrypt.hash(password, 10);
+
+	const user = await User.create({ login, password: passwordHash });
+
+	return user;
+}
 // login
 
 // logout
@@ -7,3 +20,7 @@
 // delete
 
 // edit (roles)
+
+module.exports = {
+	register,
+};
