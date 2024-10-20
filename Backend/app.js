ab20881ae2d5f9b61442054322d08c0e7c69ba5e
borboo21 <nikich621@gmail.com>
2024-10-20 17:53:58 +0300

    Реализовать вход и выход

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { register } = require('./controllers/user');
+const { register, login } = require('./controllers/user');
 const mapUser = require('./helpers/mapUser');
 
 const port = 3001;
@@ -19,6 +19,23 @@ app.post('/register', async (req, res) => {
 	} catch (error) {
 		res.send({ error: error.message || 'Unknown error' });
 	}
+});
+
+app.post('/login', async (req, res) => {
+	try {
+		const { user, token } = await login(req.body.login, req.body.password);
+
+		res.cookie('token', token, { httpOnly: true }).send({
+			error: null,
+			user: mapUser(user),
+		});
+	} catch (error) {
+		res.send({ error: error.message || 'Unknown error' });
+	}
+});
+
+app.post('/logout', (req, res) => {
+	res.cookie('token', '', { httpOnly: true }).send({});
 });
 
 mongoose
--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const generateToken = require('../helpers/generateToken');
 
 // register
 
@@ -15,7 +16,22 @@ async function register(login, password) {
 }
 // login
 
-// logout
+async function login(login, password) {
+	const user = await User.findOne({ login });
+
+	if (!user) {
+		throw new Error('User not found');
+	}
+	const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+	if (!isPasswordMatch) {
+		throw new Error('Wrong password');
+	}
+
+	const token = generateToken({ id: user.id });
+
+	return { token, user };
+}
 
 // delete
 
@@ -23,4 +39,5 @@ async function register(login, password) {
 
 module.exports = {
 	register,
+	login,
 };
